5a3a87e8952602fc13448d7749029920d229986f
Guim <guimperarnau@hotmail.com>
2016-07-27 19:26:53 +0200

    Store celebA as a serialized tensor
    This way this file can be loaded on donkey_celebA so all images are loaded at the same time in RAM.

---
+++ data/preprocess_celebA.lua
@@ -0,0 +1,58 @@
+require 'torch'
+require 'image'
+require 'nn'
+require 'io'
+
+local trainSet = {}
+local dataroot = os.getenv('DATA_ROOT')
+if dataroot == nil then
+    dataroot = 'celebA'
+end
+local data = dataroot .. '/img_align_celeba'
+
+-- Create filter
+local extensionList = {'jpg', 'png','JPG','PNG','JPEG', 'ppm', 'PPM', 'bmp', 'BMP'}
+local function isImage(filename) 
+    -- Filter all files with no image extension
+    for i=1, #extensionList do
+        if filename:find(i) then return true end
+    end
+    return false
+end
+
+local imNames = {}
+
+-- Get raw paths (including non-image paths)
+local fileIterator = io.popen("ls "..data)
+
+for filename in fileIterator:lines() do
+    if isImage(filename) then
+        imNames[#imNames+1] = filename
+    end
+end
+
+-- Open first image to get its sizes
+local im = image.load(data.. '/' .. imNames[1]):float()
+
+local x1, y1 = 30, 40
+im = image.crop(im, x1, y1, x1 + 138, y1 + 138)
+im = image.scale(im, 64, 64)
+image.save(data.. '/' ..imNames[1], im)
+
+-- Initialize output container
+local images = torch.FloatTensor(#imNames, im:size(1), im:size(2), im:size(3))
+
+images[{{1},{},{},{}}] = im
+
+for i, name in ipairs(imNames) do
+    im = image.load(data.. '/' .. imNames[i], im:size(1), 'float')
+    im = image.crop(im, x1, y1, x1 + 138, y1 + 138)
+    im = image.scale(im, 64, 64)
+    image.save(data.. '/' ..imNames[i], im)
+    images[{{i},{},{},{}}] = im
+    print(i)
+end
+
+torch.save(dataroot..'/images.dmp', images)
+torch.save(dataroot..'/imNames.dmp', imNames)
+
