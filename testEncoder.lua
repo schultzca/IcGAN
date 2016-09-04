20e1c2fcf65d0cb7e73ce66dc6659a85296f5837
Guim <guimperarnau@hotmail.com>
2016-09-04 15:51:31 +0200

    Change opt and show MSE error instead of Abs error

--- testEncoder.lua
+++ testEncoder.lua
@@ -6,17 +6,17 @@ torch.setdefaulttensortype('torch.FloatTensor')
 
 local opt = {
     batchSize = 64,         -- number of samples to produce
-    decNet = 'checkpoints/c_celebA_64_filt_Yconv1_25_net_G.t7',--'checkpoints/c_mnist_25_net_G.t7' 'checkpoints/experiment1_10_net_G.t7',-- path to the generator network
-    encNet = 'checkpoints/encoder_c_celeba_Yconv1_noTanh_20epochs.t7',--'checkpoints/encoder_c_mnist_6epochs.t7' 'checkpoints/encoder128Filters2FC_dataset2_2_6epochs.t7',
+    decNet = 'checkpoints/c_celebA_64_filt_Yconv1_noTest_wrongYFixed_24_net_G.t7',--c_celebA_64_filt_Yconv1_noTest_wrongYFixed_24_net_G, --c_mnist_-1_1_25_net_G.t7,-- path to the generator network
+    encNet = 'checkpoints/encoder_c_celeba_Yconv1_noTest_7epochs.t7',--'checkpoints/encoder_c_mnist_6epochs.t7' 'checkpoints/encoder128Filters2FC_dataset2_2_6epochs.t7',
     gpu = 1,               -- gpu mode. 0 = CPU, 1 = GPU
     nz = 100,
     customInputImage = 2,  -- 0 = no custom, only generated images used, 1 = load input image, 2 = load multiple input images
-    customImagesPath = 'celebA/img_align_celeba/', --'mnist/images', -- path used when customInputImage is 1 (path to single image) or 2 (path to folder with images)
+    customImagesPath = 'celebA/img_align_test/', --'mnist/images', -- path used when customInputImage is 1 (path to single image) or 2 (path to folder with images)
     -- Conditional GAN parameters
-    dataset = 'celebA',
+    dataset = 'celebA', -- celebA | mnist
     threshold = true, -- threshold Y vectors to binary or not
 }
-torch.manualSeed(123)
+--torch.manualSeed(123)
 
 local extensionList = {'jpg', 'png','JPG','PNG','JPEG', 'ppm', 'PPM', 'bmp', 'BMP'}
 
@@ -80,14 +80,14 @@ local outZ = output[1]; local outY = output[2]
 print("Are input and output Z equal? ", torch.all(inputZ:eq(outZ)))
 print('\tInput Z:  Mean, Stdv, Min, Max', inputZ:mean(), inputZ:std(), inputZ:min(), inputZ:max())
 print('\tOutput Z: Mean, Stdv, Min, Max', outZ:mean(), outZ:std(), outZ:min(), outZ:max())
-local error = torch.sum(torch.abs(inputZ-outZ))/(inputZ:size(1)*inputZ:size(2)*inputZ:size(3)*inputZ:size(4))
-print('\tAbsolute error per position: ', error)
+local error = torch.sum(torch.pow(torch.abs(inputZ-outZ),2))/(inputZ:size(1)*inputZ:size(2)*inputZ:size(3)*inputZ:size(4))
+print('\tMSE per position Z: ', error)
 
 print("Are input and output Y equal? ", torch.all(inputY:eq(outY)))
 print('\tInput Y:  Mean, Stdv, Min, Max', inputY:mean(), inputY:std(), inputY:min(), inputY:max())
 print('\tOutput Y: Mean, Stdv, Min, Max', outY:mean(), outY:std(), outY:min(), outY:max())
-local error = torch.sum(torch.abs(inputY-outY))/(inputY:size(1)*inputY:size(2))
-print('\tAbsolute error per position: ', error)
+local error = torch.sum(torch.pow(torch.abs(inputY-outY),2))/(inputY:size(1)*inputY:size(2))
+print('\tMSE per position Y: ', error)
 
 -- Now test if an encoded and then decoded image looks similar to the input image
 if opt.customInputImage > 0 then
@@ -145,7 +145,7 @@ if opt.threshold then
       -- celebA
       for i=1,outY:size(1) do
           for j=1,outY:size(2) do
-              local val = outY[{{i},{j}}][1][1]
+              local val = outY[i][j]
               if val > 0 then
                   outY[{{i},{j}}] = 1
               else
@@ -157,11 +157,19 @@ if opt.threshold then
 end
 -- Decode it to an output image X2
 local outX = decG:forward{outZ, outY}
-
+local combinedX = torch.Tensor(opt.batchSize*2, outX:size(2), outX:size(3), outX:size(4))
+local j = 1
+for i=1,opt.batchSize*2,2 do
+    combinedX[{{i},{},{},{}}]:copy(inputX[{{j},{},{},{}}])
+    combinedX[{{i+1},{},{},{}}]:copy(outX[{{j},{},{},{}}])
+    j = j + 1
+end
 -- Display input and output image
 disp.image(inputX, {title='Input image'})
 disp.image(outX, {title='Encoded and decoded image'})
 print("Are input and output images equal? ", torch.all(inputX:eq(outX)))
 image.save('inputImage.png', image.toDisplayTensor(inputX,0,torch.round(math.sqrt(opt.batchSize))))
 image.save('reconstructedImage.png', image.toDisplayTensor(outX,0,torch.round(math.sqrt(opt.batchSize))))
+image.save('encoder_reconstr_comparison.png', image.toDisplayTensor(combinedX,0,2,true))
+
 
